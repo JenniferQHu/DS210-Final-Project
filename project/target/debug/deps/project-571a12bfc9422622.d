e04bec7ffa0964c94fcb9947bfff3a5304e2e25e
JenniferQHu <jqh@bu.edu>
2024-12-05 15:04:09 -0500

    created main file

---
+++ project/target/debug/deps/project-571a12bfc9422622.d
@@ -0,0 +1,5 @@
+/Users/jennifer/Documents/BU/Soph - 2024 Fall/DS 210/Final Project/DS210-Final-Project/project/target/debug/deps/libproject-571a12bfc9422622.rmeta: src/main.rs
+
+/Users/jennifer/Documents/BU/Soph - 2024 Fall/DS 210/Final Project/DS210-Final-Project/project/target/debug/deps/project-571a12bfc9422622.d: src/main.rs
+
+src/main.rs:
---
+++ project/target/debug/deps/project-b08faa442b8a9342.d
@@ -0,0 +1,5 @@
+/Users/jennifer/Documents/BU/Soph - 2024 Fall/DS 210/Final Project/DS210-Final-Project/project/target/debug/deps/libproject-b08faa442b8a9342.rmeta: src/main.rs
+
+/Users/jennifer/Documents/BU/Soph - 2024 Fall/DS 210/Final Project/DS210-Final-Project/project/target/debug/deps/project-b08faa442b8a9342.d: src/main.rs
+
+src/main.rs:
