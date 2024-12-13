cffc7b1313af0aee194b475f5ec9aca3c679ea39
JenniferQHu <jqh@bu.edu>
2024-12-13 15:47:29 -0500

    Delete target folder

--- project/target/debug/deps/project-b08faa442b8a9342.d
+++
@@ -1,5 +0,0 @@
-/Users/jennifer/Documents/BU/Soph - 2024 Fall/DS 210/Final Project/DS210-Final-Project/project/target/debug/deps/libproject-b08faa442b8a9342.rmeta: src/main.rs
-
-/Users/jennifer/Documents/BU/Soph - 2024 Fall/DS 210/Final Project/DS210-Final-Project/project/target/debug/deps/project-b08faa442b8a9342.d: src/main.rs
-
-src/main.rs:
